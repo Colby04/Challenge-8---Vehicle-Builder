78cd7490559dc0dc6c0e880ec596c99f76aa9824
Colby04 <cjcodes2024@gmail.com>
2024-09-08 20:41:36 -0500

    Changes but error to npm start

---
+++ Develop/dist/classes/Cli.js
@@ -0,0 +1,255 @@
+import inquirer from "inquirer";
+import Truck from "./Truck.js";
+import Car from "./Car.js";
+import Motorbike from "./Motorbike.js";
+import Wheel from "./Wheel.js";
+export class Cli {
+    constructor(vehicles) {
+        this.selectedVehicleVin = '';
+        this.exit = false;
+        this.vehicles = vehicles;
+    }
+    static generateVin() {
+        return (Math.random().toString(36).substring(2, 15) +
+            Math.random().toString(36).substring(2, 15));
+    }
+    chooseVehicle() {
+        inquirer
+            .prompt([
+            {
+                type: 'list',
+                name: 'selectedVehicleVin',
+                message: 'Select a vehicle to perform an action on',
+                choices: this.vehicles.map((vehicle) => {
+                    let name = '';
+                    if ('vin' in vehicle &&
+                        (vehicle instanceof Car ||
+                            vehicle instanceof Truck ||
+                            vehicle instanceof Motorbike)) {
+                        const carOrTruckOrMotorbike = vehicle;
+                        name = `${carOrTruckOrMotorbike.vin} -- ${carOrTruckOrMotorbike.make} ${carOrTruckOrMotorbike.model}`;
+                        return {
+                            name: name,
+                            value: carOrTruckOrMotorbike.vin,
+                        };
+                    }
+                    return null;
+                }).filter(Boolean),
+            },
+        ])
+            .then((answers) => {
+            this.selectedVehicleVin = answers.selectedVehicleVin;
+            for (const vehicle of this.vehicles) {
+                if ('vin' in vehicle && vehicle.vin === this.selectedVehicleVin) {
+                    vehicle.printDetails();
+                }
+            }
+            this.performActions();
+        })
+            .catch((error) => {
+            console.error(error);
+        });
+    }
+    performActions() {
+        inquirer
+            .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'Select an action',
+                choices: [
+                    'Print details',
+                    'Start vehicle',
+                    'Accelerate 5 MPH',
+                    'Decelerate 5 MPH',
+                    'Stop vehicle',
+                    'Turn right',
+                    'Turn left',
+                    'Reverse',
+                    'Select or create another vehicle',
+                    'Exit',
+                    'Wheelie',
+                    'Tow',
+                ],
+            },
+        ])
+            .then((answers) => {
+            const vehicle = this.vehicles.find(v => v.vin === this.selectedVehicleVin);
+            if (!vehicle)
+                return;
+            switch (answers.action) {
+                case 'Print details':
+                    vehicle.printDetails();
+                    break;
+                case 'Start vehicle':
+                    vehicle.start();
+                    break;
+                case 'Accelerate 5 MPH':
+                    vehicle.accelerate(5);
+                    break;
+                case 'Decelerate 5 MPH':
+                    vehicle.decelerate(5);
+                    break;
+                case 'Stop vehicle':
+                    vehicle.stop();
+                    break;
+                case 'Turn right':
+                    vehicle.turn('right');
+                    break;
+                case 'Turn left':
+                    vehicle.turn('left');
+                    break;
+                case 'Reverse':
+                    vehicle.reverse();
+                    break;
+                case 'Tow':
+                    if (vehicle instanceof Truck) {
+                        this.findVehicleToTow(vehicle);
+                    }
+                    break;
+                case 'Wheelie':
+                    if (vehicle instanceof Motorbike) {
+                        vehicle.doWheelie();
+                    }
+                    break;
+                case 'Select or create another vehicle':
+                    this.startCli();
+                    return;
+                case 'Exit':
+                    this.exit = true;
+                    break;
+            }
+            if (!this.exit) {
+                this.performActions();
+            }
+        });
+    }
+    createVehicle() {
+        inquirer
+            .prompt([
+            {
+                type: 'list',
+                name: 'vehicleType',
+                message: 'Select a vehicle type',
+                choices: ['Car', 'Truck', 'Motorbike'],
+            },
+        ])
+            .then((answers) => {
+            if (answers.vehicleType === 'Car') {
+                this.createCar();
+            }
+            else if (answers.vehicleType === 'Truck') {
+                this.createTruck();
+            }
+            else if (answers.vehicleType === 'Motorbike') {
+                this.createMotorbike();
+            }
+        });
+    }
+    createCar() {
+        inquirer
+            .prompt([
+            { type: 'input', name: 'color', message: 'Enter Color' },
+            { type: 'input', name: 'make', message: 'Enter Make' },
+            { type: 'input', name: 'model', message: 'Enter Model' },
+            { type: 'input', name: 'year', message: 'Enter Year' },
+            { type: 'input', name: 'weight', message: 'Enter Weight' },
+            { type: 'input', name: 'topSpeed', message: 'Enter Top Speed' },
+        ])
+            .then((answers) => {
+            const car = new Car(Cli.generateVin(), answers.color, answers.make, answers.model, parseInt(answers.year), parseInt(answers.weight), parseInt(answers.topSpeed), []);
+            this.vehicles.push(car);
+            this.selectedVehicleVin = car.vin;
+            this.performActions();
+        });
+    }
+    createTruck() {
+        inquirer
+            .prompt([
+            { type: 'input', name: 'color', message: 'Enter Color' },
+            { type: 'input', name: 'make', message: 'Enter Make' },
+            { type: 'input', name: 'model', message: 'Enter Model' },
+            { type: 'input', name: 'year', message: 'Enter Year' },
+            { type: 'input', name: 'weight', message: 'Enter Weight' },
+            { type: 'input', name: 'topSpeed', message: 'Enter Top Speed' },
+            { type: 'input', name: 'towingCapacity', message: 'Enter Towing Capacity' },
+        ])
+            .then((answers) => {
+            const truck = new Truck(Cli.generateVin(), answers.color, answers.make, answers.model, parseInt(answers.year), parseInt(answers.weight), parseInt(answers.topSpeed), [], parseInt(answers.towingCapacity));
+            this.vehicles.push(truck);
+            this.selectedVehicleVin = truck.vin;
+            this.performActions();
+        });
+    }
+    createMotorbike() {
+        inquirer
+            .prompt([
+            { type: 'input', name: 'color', message: 'Enter Color' },
+            { type: 'input', name: 'make', message: 'Enter Make' },
+            { type: 'input', name: 'model', message: 'Enter Model' },
+            { type: 'input', name: 'year', message: 'Enter Year' },
+            { type: 'input', name: 'weight', message: 'Enter Weight' },
+            { type: 'input', name: 'topSpeed', message: 'Enter Top Speed' },
+        ])
+            .then((answers) => {
+            const motorbike = new Motorbike(Cli.generateVin(), answers.color, answers.make, answers.model, parseInt(answers.year), parseInt(answers.weight), parseInt(answers.topSpeed), [new Wheel()]);
+            this.vehicles.push(motorbike);
+            this.selectedVehicleVin = motorbike.vin;
+            this.performActions();
+        });
+    }
+    findVehicleToTow(truck) {
+        inquirer
+            .prompt([
+            {
+                type: 'list',
+                name: 'vehicleToTow',
+                message: 'Select a vehicle to tow',
+                choices: this.vehicles.map((vehicle) => {
+                    return {
+                        name: `${vehicle.vin} -- ${vehicle.make} ${vehicle.model}`,
+                        value: vehicle.vin,
+                    };
+                }),
+            },
+        ])
+            .then((answers) => {
+            const vehicleToTow = this.vehicles.find(v => v.vin === answers.vehicleToTow);
+            if (vehicleToTow && vehicleToTow !== truck) {
+                truck.tow(vehicleToTow);
+            }
+            else {
+                console.log("A truck cannot tow itself.");
+            }
+            this.performActions();
+        });
+    }
+    startCli() {
+        inquirer
+            .prompt([
+            {
+                type: 'list',
+                name: 'CreateOrSelect',
+                message: 'Would you like to create a new vehicle or perform an action on an existing vehicle?',
+                choices: ['Create a new vehicle', 'Select an existing vehicle'],
+            },
+        ])
+            .then((answers) => {
+            if (answers.CreateOrSelect === 'Create a new vehicle') {
+                this.createVehicle();
+            }
+            else {
+                this.chooseVehicle();
+            }
+        });
+    }
+}
+export function startCli() {
+    throw new Error("Function not implemented.");
+}
+export function createTruck() {
+    throw new Error("Function not implemented.");
+}
+export function createMotorbike() {
+    throw new Error("Function not implemented.");
+}
---
+++ Develop/dist/classes/Truck.js
@@ -0,0 +1,20 @@
+// Assuming Vehicle and AbleToTow are defined elsewhere in your codebase
+import Vehicle from './Vehicle';
+// The Truck class should extend the Vehicle class and should implement the AbleToTow interface
+export default class Truck extends Vehicle {
+    constructor(vin, color, make, model, year, weight, topSpeed, wheels, towingCapacity) {
+        super();
+        this.towingCapacity = towingCapacity;
+        this.vin = vin;
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.weight = weight;
+        this.topSpeed = topSpeed;
+        this.wheels = wheels;
+    }
+    tow(vehicleToTow) {
+        console.log(`Towing with capacity: ${this.towingCapacity}`);
+    }
+}
